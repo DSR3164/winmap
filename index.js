4b1fa809d4200f28f8f3a661aa9e9f0081561406
Excalibur <104138693+DSR3164@users.noreply.github.com>
2025-06-18 01:26:08 +0700

    feat: request OL directly from app

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     autoHideMenuBar: true,
-    frame: true, 
-    webPreferences: {
-      preload: __dirname + '/renderer.js'
-    }
+    frame: true,
+    webPreferences: {},
   });
 
   win.removeMenu();
-
-  win.loadURL('http://localhost:5173');
+  win.loadFile("map.html");
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
